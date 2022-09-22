5e0fc29832ac3aa527db944a88b3ef681e548ff3
Salma Yassin <syassin@uplandsoftware.com>
2022-09-22 06:07:39 +0200

    Fixing the update item

--- src/main/java/com/asset/todo/service/TodoItemServiceImpl.java
+++ src/main/java/com/asset/todo/service/TodoItemServiceImpl.java
@@ -6,6 +6,8 @@ import com.asset.todo.repository.TodoItemRepository;
 import com.asset.todo.repository.TodoUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,7 +15,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -56,9 +61,12 @@ public class TodoItemServiceImpl implements TodoItemService {
     public TodoItem update(Long id, TodoItem updatedItem) throws ChangeSetPersister.NotFoundException {
         TodoItem item = todoItemRepository.findById(id)
                 .orElseThrow(ChangeSetPersister.NotFoundException::new);
-        item.setTitle(updatedItem.getTitle());
-        item.setDescription(updatedItem.getDescription());
-        item.setDone(updatedItem.getDone());
+
+        if(updatedItem.getTitle() != null)
+            item.setTitle(updatedItem.getTitle());
+        if(updatedItem.getDescription() != null)
+            item.setDescription(updatedItem.getDescription());
+
         return todoItemRepository.save(item);
     }
 
@@ -73,6 +81,18 @@ public class TodoItemServiceImpl implements TodoItemService {
     @Override
     public Page<TodoItem> getAllByDone(Boolean done, int page, int size) {
         return todoItemRepository.findAllByDone(done, PageRequest.of(page, size));
+    }
+
+    public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for(PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        return emptyNames.toArray(new String[0]);
     }
 
 }
