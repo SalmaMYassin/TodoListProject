5a7e5e2eb45ed640996972b9f23ab6a41c4cf062
Salma Yassin <syassin@uplandsoftware.com>
2022-09-25 01:48:57 +0200

    used secret from env variables

--- src/main/java/com/asset/todo/security/JwtUtilities.java
+++ src/main/java/com/asset/todo/security/JwtUtilities.java
@@ -12,7 +12,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public final class JwtUtilities {
-    private static final Algorithm algorithm = Algorithm.HMAC256("secret".getBytes(StandardCharsets.UTF_8));
+
+    private static final String JWT_SECRET = System.getProperty("JWT_SECRET","secret");
+    private static final Algorithm algorithm = Algorithm.HMAC256(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
     private static final String BEARER = "Bearer ";
     private static final String USER = "USER";
 
